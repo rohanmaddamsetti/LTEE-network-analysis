1e246b3972cc2d65ce288e983b7034bcceea75c7
Rohan Maddamsetti <rohan.maddamsetti@gmail.com>
2021-01-19 22:50:13 -0500

    playing with fitness figure

--- resilience-analysis.R
+++ resilience-analysis.R
@@ -586,25 +586,12 @@ mean(OnlyDel.KOed.50K.clone.A.cong.PPI.degrees$Degree)
 mean(NoDel.KOed.50K.clone.A.cong.PPI.degrees$Degree)
 
 ########################################################
-## Compare population fitness and change in fitness (Wiser et al. 2013)
+## Compare population fitness (Wiser et al. 2013)
 ## to the resilience of genomes isolated at those time points.
 ## Overall, I find this result inconclusive.
 ## measuring the actual fitnesses of the relevant clones
 ## would give a better picture.
 
-calc.dFitness.dt <- function(wiser.df) {
-
-    calc.dFitness.dt.helper <- function(pop.df) {
-        df <- pop.df %>%
-            mutate(Fitness.diff = mean.Fitness - lag(mean.Fitness)) %>%
-            mutate(Generation.diff = Generation - lag(Generation)) %>%
-            mutate(dFitness.dt = Fitness.diff/Generation.diff)
-    }
-    
-    wiser.df %>%
-        split(.$population) %>%
-        map_dfr(.f = calc.dFitness.dt.helper)
-}
 
 wiser.data <- read.csv("../data/Concatenated.LTEE.data.all.csv") %>%
     ## get time units to match the resilience dataframes.
@@ -616,9 +603,7 @@ wiser.data <- read.csv("../data/Concatenated.LTEE.data.all.csv") %>%
     ## calculate the average fitness over replicates
     summarize(mean.Fitness = mean(Fitness)) %>%
     ungroup() %>%
-    select(Generation, population, mean.Fitness) %>%
-    ## calculate delta Fitness/delta Generation
-    calc.dFitness.dt()
+    select(Generation, population, mean.Fitness)
 
 
 cong.resilience.with.fitness.df <- big.cong.resilience.df %>%
@@ -628,7 +613,9 @@ cong.resilience.with.fitness.df <- big.cong.resilience.df %>%
            population, Generation) %>%
     group_by(dataset, strain, population, Generation) %>%
     summarize(mean.resilience = mean(resilience)) %>%
-    left_join(wiser.data)
+    ungroup() %>%
+    left_join(wiser.data) %>%
+    filter(complete.cases(.))
 
 zitnik.resilience.with.fitness.df <- big.zitnik.resilience.df %>%
     filter(run_type == "LTEE_genome_resilience") %>%
@@ -637,16 +624,13 @@ zitnik.resilience.with.fitness.df <- big.zitnik.resilience.df %>%
            population, Generation) %>%
     group_by(dataset, strain, population, Generation) %>%
     summarize(mean.resilience = mean(resilience)) %>%
-    left_join(wiser.data)
+    ungroup() %>%
+    left_join(wiser.data) %>%
+    filter(complete.cases(.))
 
 
 cong.fitness.plot <- ggplot(cong.resilience.with.fitness.df,
                             aes(x=mean.Fitness,y=mean.resilience,color=population)) +
-    geom_point() +
-    theme_classic()
-
-cong.dFitness.dt.plot <- ggplot(cong.resilience.with.fitness.df,
-                            aes(x=dFitness.dt,y=mean.resilience,color=population)) +
     geom_point() +
     theme_classic()
 
@@ -655,13 +639,32 @@ zitnik.fitness.plot <- ggplot(zitnik.resilience.with.fitness.df,
     geom_point() +
     theme_classic()
 
-zitnik.dFitness.dt.plot <- ggplot(zitnik.resilience.with.fitness.df,
-                            aes(x=dFitness.dt,y=mean.resilience,color=population)) +
+
+cong.fitness.plot2 <- ggplot(cong.resilience.with.fitness.df,
+                             aes(x = Generation,
+                                 y = mean.Fitness,
+                                 color = mean.resilience)) +
     geom_point() +
-    theme_classic()
+    theme_classic() +
+    scale_color_viridis_c(option = "plasma", direction = -1)
 
+zitnik.fitness.plot2 <- ggplot(zitnik.resilience.with.fitness.df,
+                               aes(x = Generation,
+                                   y = mean.Fitness,
+                                   color = mean.resilience)) +
+    geom_point() +
+    theme_classic() +
+    scale_color_viridis_c(option = "plasma", direction = -1)
 
-fitness.Fig <- plot_grid(cong.fitness.plot, cong.dFitness.dt.plot,
-                         zitnik.fitness.plot, zitnik.dFitness.dt.plot,
+fitness.Fig <- plot_grid(cong.fitness.plot2,
+                         zitnik.fitness.plot2,
+                         cong.fitness.plot,
+                         zitnik.fitness.plot,
                          labels=c('A','B','C','D'),
-                         ncol=2)
+                         nrow=2)
+
+cor.test(zitnik.resilience.with.fitness.df$mean.resilience,
+         zitnik.resilience.with.fitness.df$mean.Fitness)
+
+cor.test(cong.resilience.with.fitness.df$mean.resilience,
+         cong.resilience.with.fitness.df$mean.Fitness)
